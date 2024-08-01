d58267cae71c7541e7519bb72adb6efaf9c57367
Buhwi <149663256+Buhwi@users.noreply.github.com>
2024-08-01 15:50:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.urandom(24)
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'mysql 사용자 이름'
-    MYSQL_PASSWORD = 'mysql password'
+    MYSQL_USER = 'mysql 사용자 이름'     # 본인 mysql id 입력
+    MYSQL_PASSWORD = 'mysql password' # 본인 mysql pw 입력
     MYSQL_DB = 'login'
