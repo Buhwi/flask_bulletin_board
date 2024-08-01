404f95b1329745087a821f5c2856f15865bb26c0
Buhwi <hwi01304@gamil.com>
2024-08-01 15:45:11 +0900

    window에서 구동 가능하게 수정

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.urandom(24)
     MYSQL_HOST = 'localhost'
-<<<<<<< HEAD
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'newisql@5506'
-=======
     MYSQL_USER = 'mysql 사용자 이름'
     MYSQL_PASSWORD = 'mysql password'
->>>>>>> d0bdff1 (window에서 구동 가능하게 수정)
     MYSQL_DB = 'login'
